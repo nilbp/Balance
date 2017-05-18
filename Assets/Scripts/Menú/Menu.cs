6d7ff6bdfc43b532ef6fdde70b5c8ba7956f463f
Unknown <nil.bellmunt@gmail.com>
2017-05-18 16:02:02 +0200

    Intent menú script

---
+++ Assets/Scripts/Menú/Menu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menú : MonoBehaviour {
+
+	public GameObject loadingimage;
+
+	public void LoadScene(int level) 
+	{
+		loadingimage.SetActive (true);
+		Application.LoadLevel (level);
+	}
+
+}
