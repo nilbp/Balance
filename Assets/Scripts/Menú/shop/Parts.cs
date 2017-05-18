825d1bdcb59abd277973a5649d95ef2384ee19b3
Unknown <nil.bellmunt@gmail.com>
2017-05-18 18:57:15 +0200

    Estètica de la tenda i començament d'script

---
+++ Assets/Scripts/Menú/shop/Parts.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parts: MonoBehaviour {
+
+
+
+	public void parts(string hola)
+	{
+		GetComponent<SpriteRenderer> ().color = new Color (200f, 200f, 100f, 255f);
+	}
+}
