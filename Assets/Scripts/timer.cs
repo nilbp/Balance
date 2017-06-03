b588fb7f034cd95433bc69ca600c5d0df4c6a42a
Unknown <adamtubauvilar@enti.cat>
2017-06-04 00:03:54 +0200

    començament finishgame

---
+++ Assets/Scripts/timer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timer : MonoBehaviour {
+
+	public static float timeSinceLevelLoad=60;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(timeSinceLevelLoad==0){
+			GameOver ();
+	}
+}
+
+	void GameOver(){
+		Application.LoadLevel ("FinishGame");
+	}
+}
